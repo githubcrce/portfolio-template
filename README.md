49729d60432967096dc7f41f7ae67e6e6e5c4ba4
yashsdeshmukh <ydeshmukh0093@gmail.com>
2021-07-13 15:11:48 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Just change src/yourdata.js to get your personal portfolio. Feel free to use it as-is or customize it as much as you want.
+
+## To view live example 
+https://yashsdeshmukh.netlify.app/
+
+## How to use
+# Clone this repository
+$ git clone https://github.com/githubcrce/portfolio-template.git
+
+# Go into the repository
+$ cd portfolio-template
+
+# Install dependencies
+$ yarn
+
+# Start's development server
+$ gatsby develop
+
+## Consider deploying your portfolio to github pages or Netlify
+
+
---
+++ src/yourdata.js
@@ -0,0 +1,184 @@
+// Skills Icons
+import androidIcon from "./images/android.svg"
+import reactIcon from "./images/react.svg"
+import awsIcon from "./images/aws.svg"
+import pythonIcon from "./images/python.svg"
+import gitIcon from "./images/git.svg"
+import javaIcon from "./images/java.svg"
+import cssIcon from "./images/css.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "FirstName",
+  displayPicture: "https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png",
+  headerTagline: [
+    //Line 1 For Header
+    "An engineer who",
+    //Line 2 For Header
+    "codes and commits",
+    //Line 3 For Header
+    "on GithubCRCE",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "Drop an email with your queries",
+
+  //Contact Email
+  contactEmail: "youremail@email.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Project One", //Project Title - Add Your Project Title Here
+      para:
+        "Description of the same", // Add Your description Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/githubcrce",
+    },
+    {
+      title: "Project Two", //Project Title - Add Your Project Title Here
+      para:
+        "Description of the same", // Add Your Service Type Here
+      //Project Image - Add Your Description type here
+      imageSrc:
+        "https://images.unsplash.com/photo-1495195134817-aeb325a55b65?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/githubcrce",
+    },
+    {
+      title: "Project Three", //Project Title - Add Your Project Title Here
+      para:
+        "Description of the same", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1582502580092-0dc3088c7aeb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/githubcrce",
+    },
+    {
+      title: "Project Four", //Project Title - Add Your Project Title Here
+      para:
+        "Description of the same", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/githubcrce",
+    },
+    {
+      title: "Project Five", //Project Title - Add Your Project Title Here
+      para:
+        "Description of the same", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+      "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/githubcrce",
+    },
+    {
+      title: "Project Six", //Project Title - Add Your Project Title Here
+      para:
+        "Description of the same project enhanced and beautified to look pretty", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/githubcrce",
+    },
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "I am a student of computer science who believes in using React for building web",
+  aboutParaTwo:
+    "I am a curious and persistent person with a series of 10 pointers throughout my college life and I love scoring 10s again and again .... LOL thats not me",
+  aboutParaThree:
+    "I have the capacity to skip any work irrespective of it's complexity",
+  aboutImage:
+    "https://images.unsplash.com/photo-1517411032315-54ef2cb783bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=701&q=80",
+
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: javaIcon,
+    },
+    {
+      img: androidIcon,
+    },
+    {
+      img: javaIcon,
+    },
+    {
+      img: androidIcon,
+    },
+    {
+      img: gitIcon,
+    },
+    {
+      img: awsIcon,
+    },
+    {
+      img: pythonIcon,
+    },
+    {
+      img: awsIcon,
+    },
+    {
+      img: pythonIcon,
+    },
+    
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Background Section --------------------------
+
+  backgroundHeading: "Background",
+  Designation:"Data analytics Expert",
+  Company:"Facebook Incorporations",
+  backgroundPara1: "Spying on people",
+  backgroundPara2: "Stealing their data",
+  backgroundPara3: "Pumping profits",
+  
+  // End Background Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create our next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { 
+      img: githubIcon, url: "https://github.com/yashsdeshmukh" 
+    },
+  ],
+
+  // End Contact Section ---------------
+}
